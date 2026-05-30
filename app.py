da48e8d65f30b41d756e1c1e2a63b97b3b4df595
Syed-Rayyan-Gilani <srayyan360@gmail.com>
2026-05-30 17:02:23 +0530

    Flask connected to MySQL database successfully

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Database connection
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:srg@2005@localhost/smart_expense_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:srg%402005@localhost/smart_expense_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'smartexpense123'
 
@@ -17,6 +17,14 @@ login_manager = LoginManager(app)
 @app.route("/")
 def home():
     return "Smart Expense System is running!"
+
+@app.route('/test-db')
+def test_db():
+    try:
+        db.session.execute(db.text('SELECT 1'))
+        return 'Database connected successfully!'
+    except Exception as e:
+        return f'Database connection failed: {str(e)}'
 
 if __name__ == "__main__":
     app.run(debug=True)
